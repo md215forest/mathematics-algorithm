60d5247f8f8fcd72410582a5181237354d674d9c
cm118880 <mori.daiki@paypay-card.co.jp>
2024-11-29 14:50:43 +0900

    refact:recursion euclidean algorithm

--- src/algorithm/euclidean_algorithm.rs
+++ src/algorithm/euclidean_algorithm.rs
@@ -1,17 +1,6 @@
-use std::cmp::{max, min};
-
 pub fn calculate(n: i32, m: i32) -> i32 {
-    let mut larger = max(n, m);
-    let mut smaller = min(n, m);
-    while larger >= 1 && smaller >= 1 {
-        if smaller < larger {
-            larger = larger % smaller;
-        } else {
-            smaller = smaller % larger
-        }
+    if m == 0 {
+        return n;
     }
-    if smaller >= 1 {
-        return smaller;
-    }
-    larger
+    return calculate(m, n % m);
 }
--- src/algorithm/mod.rs
+++ src/algorithm/mod.rs
@@ -1,7 +1,7 @@
 // pub mod binary_search;
 // pub mod bit_full_serach;
-// pub mod euclidean_algorithm;
+pub mod euclidean_algorithm;
 // pub mod full_search;
 // pub mod monte_carlo;
 // pub mod primality_test;
-pub mod sort;
+// pub mod sort;
