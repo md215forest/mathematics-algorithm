b4f6afbb87790a619ea7a9495f499a2b90bba339
cm118880 <mori.daiki@paypay-card.co.jp>
2024-11-29 09:11:17 +0900

    refact: sort method

--- src/algorithm/sort.rs
+++ src/algorithm/sort.rs
@@ -1,16 +1,11 @@
 pub fn sort(mut v: Vec<i32>) -> Vec<i32> {
     for i in 0..v.len() {
-        let mut min = 0;
-        let mut min_value = 0;
+        let mut min = i;
+        let mut min_value = v[i];
         for j in i..v.len() {
-            if j == i {
+            if v[j] < min_value {
                 min = j;
                 min_value = v[j];
-            } else {
-                if v[j] < min_value {
-                    min = j;
-                    min_value = v[j];
-                }
             }
         }
         v.swap(i, min);
