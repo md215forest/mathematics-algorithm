a22e7cd0182decba710b1fd9cf4c61c5f55606cc
cm118880 <mori.daiki@paypay-card.co.jp>
2024-12-04 23:38:40 +0900

    feat: repeated squaring

--- src/main.rs
+++ src/main.rs
@@ -57,11 +57,14 @@ fn main() {
     // let _primes = algorithm::sieve_of_erastosthenes::get_primes(1000000);
     // println!("素数のリスト: {:?}", primes);
 
-    let connected = algorithm::depth_first_search::run();
-    println!("連結しているか: {}", connected);
+    // let connected = algorithm::depth_first_search::run();
+    // println!("連結しているか: {}", connected);
 
     // let connected = algorithm::breadth_first_search::run();
     // println!("連結しているか: {}", connected);
+
+    let remainder = algorithm::repeated_squaring::run2(3, 40);
+    println!("余り: {}", remainder);
 }
 
 fn _get_array(n: i32, max: i32) -> Vec<i32> {
